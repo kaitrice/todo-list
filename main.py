af16f7039cd6a9391e88c1971caff12bb8b22757
Kait Rice <kaitnrice@gmail.com>
2025-10-06 22:05:46 -0700

    enable use of lists

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 tasks = []
 
-def add(task):
-  tasks.append(task)
-  return
+def add(task_list):
+  for task in task_list:
+    tasks.append(task)
 
-def remove(task):
-  tasks.remove(task)
-  return
+def remove(task_list):
+  for task in task_list:
+    if task in tasks:
+      tasks.remove(task)
 
-def complete(task):
+def complete(task_list):
   return
 
 def list():
-  print(tasks)
-  return
+  print(f"\n{tasks}\n")
 
 def print_func():
   print("\nCOMMANDS")
-  print(" a <task> : add task")
-  print(" d <task> : delete task")
-  print(" c <task> : complete task")
+  print(" a <task> ... : add task(s)")
+  print(" d <task> ... : delete task(s)")
+  print(" c <task> ... : complete task(s)")
   print(" l : list tasks")
   print(" ? : print commands")
   print(" q : quit\n")
@@ -34,19 +34,19 @@ while cmd != 'q':
   match cmd:
     case 'a':
       if len(items) >= 2:
-        add(items[1])
+        add(items[1:])
       else:
-        print("invalid syntax: a <task>")
+        print("invalid syntax: a <task> ...")
     case 'd':
       if len(items) >= 2:
-        remove(items[1])
+        remove(items[1:])
       else:
-        print("invalid syntax: d <task>")
+        print("invalid syntax: d <task> ...")
     case 'c':
       if len(items) >= 2:
-        complete(items[1])
+        complete(items[1:])
       else:
-        print("invalid syntax: c <task>")
+        print("invalid syntax: c <task> ...")
     case 'l':
       list()
     case '?':
