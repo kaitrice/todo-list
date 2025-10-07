2b88db25b633db890d4620546c2f57e5b5006eda
Kait Rice <kaitnrice@gmail.com>
2025-10-06 22:49:49 -0700

    format print

--- utils.py
+++ utils.py
@@ -20,7 +20,10 @@ def complete(task_list):
     tasks[task] = True
 
 def print_tasks():
-  print(f"\n{tasks}\n")
+  print("\n")
+  for key, value in tasks.items():
+    print(f"{key}: {value}")
+  print("\n")
 
 def print_func():
   print("\nCOMMANDS")
